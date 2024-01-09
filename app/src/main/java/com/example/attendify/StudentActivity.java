c0214062b136f6f44d032f2241d83377bc3f07e4
botfigther02 <abuhurayraniloy02@gmail.com>
2024-01-09 22:12:36 +0600

    .

--- app/src/main/java/com/example/attendify/StudentActivity.java
+++ app/src/main/java/com/example/attendify/StudentActivity.java
@@ -1,20 +1,31 @@
 package com.example.attendify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toolbar;
+
 
 public class StudentActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    private String className;
+    private String subjectName;
+    private int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+        Intent intent = getIntent();
+        className = intent.getStringExtra("className");
+        subjectName = intent.getStringExtra("subjectName");
+        position = intent.getIntExtra("position", -1);
+        setToolbar();
     }
 
     private void setToolbar() {
@@ -24,8 +35,9 @@ public class StudentActivity extends AppCompatActivity {
         ImageButton back = toolbar.findViewById(R.id.back);
         ImageButton save = toolbar.findViewById(R.id.save);
 
-        title.setText("Attendify");
+        title.setText("className");
+        subtitle.setText("subjectName");
 
-
+        back.setOnClickListener(v-> onBackPressed());
     }
 }
